c84beaac2c0e03e15bee153ecd064552a3f24490
Bruno Thomsen <bruno.thomsen@gmail.com>
2024-06-05 12:46:40 +0200

    setup-router: add extra trace
    It's now easier to follow output trace.
    
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>

--- bash/setup-router.sh
+++ bash/setup-router.sh
@@ -5,7 +5,7 @@
 set -e # Exit script if any statement returns a non-true value.
 set -u # Exit script if using an uninitialised variable.
 
-VERSION="0.2.0-dev"
+PKG_VERSION="0.2.0-dev"
 
 ################################################################################
 # Mandatory Configuration
@@ -71,13 +71,50 @@ fi
 # Implementation
 ################################################################################
 
+source /etc/os-release
+
+GREEN='\033[0;32m'
+BLUE='\033[94m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+BOLD='\033[1m'
+GRAY='\033[2m'
+NC='\033[0m' # No Color
+
+print_header()
+{
+    echo -e "${BOLD}========================================${NC}"
+    echo -e "${BLUE}${FUNCNAME[1]}${NC}"
+    echo -e "${GRAY}========================================${NC}"
+}
+
 start_check()
 {
-    echo "home-router v${VERSION}"
+    echo -e "${GREEN}home-router v${PKG_VERSION}${NC}"
+    print_header
+
+    if [ "${NAME}" != "Fedora Linux" ]; then
+        echo -e "${YELLOW}Warning: Untested distribution (${NAME})${NC}"
+    fi
+
+    echo "Distribution: ${PRETTY_NAME}"
+    echo "WAN interface: ${WAN_INTERFACE}"
+    echo "LAN interface: ${LAN_INTERFACE}"
+    echo "Bridge interface: ${BRIDGE_INTERFACE}"
+    echo "LAN network: ${LAN_NETWORK}"
+    echo "DHCP client pool: ${DHCP_LOW} - ${DHCP_HIGH}"
+    echo "DHCP client dns: ${DNS_SERVER_PRIMARY}, ${DNS_SERVER_SECONDARY}"
+
+    if [ -z "${WAN_INTERFACE}" ] || [ -z "${LAN_INTERFACE}" ]; then
+        echo -e "${RED}Error: Missing interface information${NC}"
+        exit 1
+    fi
 }
 
 install_packages()
 {
+    print_header
+
     DNF_INSTALL_PKG=( dnf-automatic patch gettext-envsubst ipcalc )
     if [ "${EXPERIMENTAL_KEA_DHCP_SERVER}" -eq 1 ]; then
         DNF_INSTALL_PKG+=( kea )
@@ -89,6 +126,8 @@ install_packages()
 
 setup_auto_update()
 {
+    print_header
+
     dnf_override_path="/etc/systemd/system/dnf-automatic-install.timer.d"
     mkdir -p "${dnf_override_path}"
     cp -v "conf.d/dnf-override.conf" "${dnf_override_path}"
@@ -100,6 +139,8 @@ setup_auto_update()
 
 setup_wan_firewall()
 {
+    print_header
+
     firewall-cmd --set-default-zone=external
     firewall-cmd --permanent --zone=external --add-service=dhcpv6-client
     firewall-cmd --permanent --zone=external --remove-service=ssh
@@ -107,6 +148,8 @@ setup_wan_firewall()
 
 setup_lan_firewall()
 {
+    print_header
+
     firewall-cmd --permanent --zone=internal --remove-service=dhcpv6-client
     firewall-cmd --permanent --zone=internal --remove-service=samba-client
     firewall-cmd --permanent --zone=internal --add-service=dhcp
@@ -117,6 +160,8 @@ setup_lan_firewall()
 
 setup_bridge()
 {
+    print_header
+
     if PAGER="" nmcli connection show "${BRIDGE_INTERFACE}" > /dev/null 2>&1; then
         nmcli connection delete "${BRIDGE_INTERFACE}"
     fi
@@ -138,6 +183,8 @@ setup_bridge()
 
 setup_isc_dhcp_server()
 {
+    print_header
+
     export "$(ipcalc --address ${LAN_NETWORK})"
     export "$(ipcalc --network ${LAN_NETWORK})"
     export "$(ipcalc --netmask ${LAN_NETWORK})"
@@ -150,11 +197,15 @@ setup_isc_dhcp_server()
 
 remove_isc_dhcp_server()
 {
+    print_header
+
     dnf remove -y dhcp-server
 }
 
 setup_kea_dhcp_server()
 {
+    print_header
+
     export "$(ipcalc --address ${LAN_NETWORK})"
     export "$(ipcalc --network ${LAN_NETWORK})"
     export "$(ipcalc --prefix ${LAN_NETWORK})"
@@ -165,16 +216,22 @@ setup_kea_dhcp_server()
 
 remove_kea_dhcp_server()
 {
+    print_header
+
     dnf remove -y kea
 }
 
 setup_ssh_server()
 {
+    print_header
+
     cp -v "conf.d/08-home-router-sshd.conf" "/etc/ssh/sshd_config.d/"
 }
 
 firewall_status()
 {
+    print_header
+
     firewall-cmd --reload
     firewall-cmd --info-zone=external
     firewall-cmd --info-zone=internal
@@ -187,6 +244,7 @@ setup_bridge
 setup_wan_firewall
 setup_lan_firewall
 if [ "${EXPERIMENTAL_KEA_DHCP_SERVER}" -eq 1 ]; then
+    echo -e "${YELLOW}Warning: Experimental KEA DHCP server enabled${NC}"
     remove_isc_dhcp_server
     setup_kea_dhcp_server
 else
