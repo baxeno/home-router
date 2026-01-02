a25a7e8633b02e4131df935431c6891d65b025ee
Bruno Thomsen <bruno.thomsen@gmail.com>
2026-01-02 18:29:12 +0100

    ansible: add validate-syntax.sh script
    Help catch issues before VM testing.
    
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>

---
+++ ansible/validate-syntax.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e # Exit script if any statement returns a non-true value.
+set -u # Exit script if using an uninitialised variable.
+
+INVENTORY_YAML_FILES=(
+    localhost.yml
+    test-alma10.yml
+)
+
+# Additional information is printed with 2 x verbose
+for inventory_file in "${INVENTORY_YAML_FILES[@]}"; do
+    ansible-playbook \
+        --syntax-check \
+        --verbose \
+        --verbose \
+        --inventory "inventory/${inventory_file}" \
+        home-router.yml
+done
+
+printf "\nValidated successful in %s seconds\n" "$SECONDS"
+
