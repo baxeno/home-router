b91e74469f92326de5d3a80c482eb3172005c892
Bruno Thomsen <bruno.thomsen@gmail.com>
2026-08-14 13:49:41 +0200

    ansible: validate syntax of all inventory files
    Dynamically find new inventory files.
    
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>

--- ansible/validate-syntax.sh
+++ ansible/validate-syntax.sh
@@ -4,20 +4,19 @@
 set -e # Exit script if any statement returns a non-true value.
 set -u # Exit script if using an uninitialised variable.
 
-INVENTORY_YAML_FILES=(
-    localhost.yml
-    test-alma10.yml
-)
+PLAYBOOK_YAML_FILE="home-router.yml"
+mapfile -d $'\0' INVENTORY_YAML_FILES < <(find inventory/ -type f -name "*.yml" -print0)
 
 # Additional information is printed with 2 x verbose
 for inventory_file in "${INVENTORY_YAML_FILES[@]}"; do
+    echo "Validating playbook: ${PLAYBOOK_YAML_FILE} on inventory: ${inventory_file}"
     ansible-playbook \
         --syntax-check \
         --verbose \
         --verbose \
-        --inventory "inventory/${inventory_file}" \
-        home-router.yml
+        --inventory "${inventory_file}" \
+        "${PLAYBOOK_YAML_FILE}"
+    echo "----------------------------------------"
 done
 
 printf "\nValidated successful in %s seconds\n" "$SECONDS"
-
